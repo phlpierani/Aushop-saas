5b78397a11593fe40f6b6fb0b11a41dc26db5251
Pedro Henrique Lourenço Pierani <130299627+phlpierani@users.noreply.github.com>
2025-10-13 21:36:50 -0300

    Configurações iniciais

---
+++ Aushop-saas/src/main/java/com/pierani/saaid/Aushop_saas/AushopSaasApplication.java
@@ -0,0 +1,13 @@
+package com.pierani.saaid.Aushop_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AushopSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AushopSaasApplication.class, args);
+	}
+
+}
---
+++ Aushop-saas/src/test/java/com/pierani/saaid/Aushop_saas/AushopSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierani.saaid.Aushop_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AushopSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
